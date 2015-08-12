90b38622b5aa6c1c9284daf0f569d4162207409f
Bulat <sadikovba@gmail.com>
2015-08-12 17:25:25 +0300

    Add method to save news in db

--- news/save_news.inc.php
+++ news/save_news.inc.php
@@ -1,4 +1,12 @@
 <?php
+$title = $news->clearStr($_POST['title']);
+$category = $news->clearInt($_POST['category']);
+$description = $news->clearStr($_POST['description']);
+$source = $news->clearStr($_POST['source']);
 if (isset($_POST['title']) and isset($_POST['category']) and isset($_POST['description']) and isset($_POST['source'])) {
-
+  if (!$news->saveNews($title, $category, $category, $source)) {
+    $errMsg = "Ошибка при добавлении новости!";
+  } else {
+      header("Location: news.php");
+  }
 } else $errMsg = "Заполните все поля формы!";
